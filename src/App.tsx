298d68a9449345bf5e53a2f39f381f5e9ead8b3e
Nicolas Toconas <ntoconas@lexim.solutions>
2024-09-09 19:36:18 -0300

    Agregado los datos a consumir, primeros types y componente MenuItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { menuItems } from "./data/db"
+import MenuItem from "./components/MenuItem"
+
+
 function App() {
 
   return (
     <>
-      <h1>Hola mundo</h1>
-      <p>hola mundo 2</p>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+
+          {menuItems.map(item => (
+            <MenuItem
+              key={item.id}
+            />
+          ))}
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>    
+      </main>
     </>
   )
 }
